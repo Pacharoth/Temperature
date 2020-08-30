fd79e0f015b087807bfd6678e1fbd9c7f4aed42a
Pacharoth <pacharothroeun01@gmail.com>
2020-08-30 11:35:33 +0700

    server arduino side

--- temperature/adminapp/views.py
+++ temperature/adminapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from adminapp.forms import loginForm
 
 # Create your views here.
 def loginAdmin(request):
+    form = loginForm(request.POST or None)
+    
     return render(request,"adminauth/login.html")
     
