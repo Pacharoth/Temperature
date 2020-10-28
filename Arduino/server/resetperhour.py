ec90db53a45d985e1b30ddf37c1cac79e559066b
roeun <pacharothroeun01@gmail.com>
2020-10-28 14:50:51 +0700

    another version

--- Arduino/server/resetperhour.py
+++ Arduino/server/resetperhour.py
@@ -2,9 +2,5 @@ import time
 import requests
 while(True):
     time.sleep(3600)
-<<<<<<< HEAD
     r =requests.get("https://192.168.51.212/resetperhour/",verify=False)
-=======
-    r =requests.get("https://192.168.51.212/resetperhour/")
->>>>>>> 0cdb3dfeb662914fc994753152faa4895c97e6a0
     
--- Arduino/server/sendmail.py
+++ Arduino/server/sendmail.py
@@ -2,12 +2,8 @@ import time
 import requests
 while(True):
     time.sleep(2)
-<<<<<<< HEAD
     r=requests.get("https://192.168.51.212/sendmail",verify=False)
     print(r)
-=======
-    r =requests.get("http://127.0.0.1:8000/sendmail/")
->>>>>>> 0cdb3dfeb662914fc994753152faa4895c97e6a0
     p=r.json()['data']
     for i in p:
         if i>25:
